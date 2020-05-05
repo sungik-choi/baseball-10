443e963ddaa237de1e4b011258ee36a7bb178bf9
hyunjun <guswns1659@gmail.com>
2020-05-05 15:54:04 +0900

    [#1] Feat : 프로젝트 초기 셋팅
    Fixed #1
    - 팀 그라운드 룰 설정
    - 스프링부트 프로젝트 생성

---
+++ BE/src/main/java/com/codesquad/baseball10/Baseball10Application.java
@@ -0,0 +1,13 @@
+package com.codesquad.baseball10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Baseball10Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Baseball10Application.class, args);
+    }
+
+}
