e98b573188f52d62e03e0c9e4d9cd4b198734ab5
hyunjun <guswns1659@gmail.com>
2020-05-05 17:15:39 +0900

    [#1] Feat : 패키지 구조 설정 및 Controlelr test
    - HelloController 생성 및 Test.
    - Test 실패 시 bulid.gradle 설정 변경

--- BE/src/test/java/com/codesquad/baseball10/Baseball10ApplicationTests.java
+++ BE/src/test/java/com/codesquad/baseball10/Baseball10ApplicationTests.java
@@ -1,13 +1,44 @@
 package com.codesquad.baseball10;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@SpringBootTest
-class Baseball10ApplicationTests {
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class Baseball10ApplicationTests {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    private final Logger logger = LoggerFactory.getLogger(Baseball10ApplicationTests.class);
 
     @Test
-    void contextLoads() {
+    public void contextLoads() {
+        assertThat(restTemplate).isNotNull();
+        assertThat(logger).isNotNull();
     }
 
+    @Test
+    public void HelloControllerTest() {
+        String hello = "hello";
+        String url = "http://localhost:" + port + "/" + hello;
+        ResponseEntity<String> responseEntity = restTemplate.getForEntity(url, String.class);
+
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(responseEntity.getBody()).isEqualTo(hello);
+    }
 }
