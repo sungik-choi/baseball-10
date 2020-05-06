c818c030f4d27d5d608eb8971a54ecf59d6fe419
최장호 <hmchoi651@naver.com>
2020-05-06 19:54:00 +0900

    [#8] Docs : File name 변경

--- FE/baseball/src/mock/gameList.js
+++
@@ -1,9 +0,0 @@
-const baseballData = {
-  gameList: [
-    { home: "Lions", away: "BEARS" },
-    { home: "Eagles", away: "Giants" },
-    { home: "Dinos", away: "Tigers" },
-  ],
-};
-
-export default baseballData;
