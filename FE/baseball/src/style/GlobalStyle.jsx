d7a90c3da39cb283b74d055cf4a261f5d9fc191c
최장호 <hmchoi651@naver.com>
2020-05-06 15:58:49 +0900

    [#8] Style : Prettier Code 컨벤션 수정

--- FE/baseball/src/style/GlobalStyle.jsx
+++ FE/baseball/src/style/GlobalStyle.jsx
@@ -1,17 +1,17 @@
-import { createGlobalStyle, css } from 'styled-components';
-import reset from 'styled-reset';
+import { createGlobalStyle, css } from "styled-components";
+import reset from "styled-reset";
 
 const fonts = css`
   @font-face {
-    font-family: 'Regular';
-    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2001@1.1/CookieRun-Regular.woff') format('woff');
+    font-family: "Regular";
+    src: url("https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2001@1.1/CookieRun-Regular.woff") format("woff");
     font-weight: normal;
     font-style: normal;
   }
 
   @font-face {
-    font-family: 'Bold';
-    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_twelve@1.0/CookieRunOTF-Bold00.woff') format('woff');
+    font-family: "Bold";
+    src: url("https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_twelve@1.0/CookieRunOTF-Bold00.woff") format("woff");
     font-weight: normal;
     font-style: normal;
   }
