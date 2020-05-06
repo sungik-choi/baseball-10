4199234cf2a85c20e47d28fb54921736c7e92ae2
Sungik Choi <tjddlr92@gmail.com>
2020-05-06 15:56:53 +0900

    [#8] Docs: prettier 설정 파일 추가
    - 쌍따옴표만 허용되게

--- FE/baseball/src/components/login/Login.jsx
+++ FE/baseball/src/components/login/Login.jsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
 const Login = () => {
   return <LoginWrap>Login</LoginWrap>;
 };
 
 const LoginWrap = styled.div`
-  font-family: 'Regular';
+  font-family: "Regular";
   border: 1px solid red;
 `;
 
