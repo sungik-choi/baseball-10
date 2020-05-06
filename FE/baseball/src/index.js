c84b8d1a59c4c09dfcb9a797533436d9cca7a5c7
최장호 <hmchoi651@naver.com>
2020-05-06 12:19:11 +0900

    [#6] Feat : Route 기본 설정

--- FE/baseball/src/index.js
+++ FE/baseball/src/index.js
@@ -1,13 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
   document.getElementById("root")
 );
 
