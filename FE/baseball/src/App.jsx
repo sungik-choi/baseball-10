e7cc059a5aba9daf1399f9d13949dba5e68122df
최장호 <hmchoi651@naver.com>
2020-05-06 20:05:02 +0900

    [#8] Docs :  App.jsx 상위 Component 로 baseballProvider 추가

--- FE/baseball/src/App.jsx
+++ FE/baseball/src/App.jsx
@@ -6,16 +6,18 @@ import Login from "./components/login/Login.jsx";
 import GameList from "./components/gamelist/GameList.jsx";
 import PlayGround from "./components/playground/PlayGround.jsx";
 
+import BaseballProvider from "./components/BaseballProvider.jsx";
+
 const App = () => {
   return (
-    <>
+    <BaseballProvider>
       <GlobalStyle />
       <Switch>
         <Route path="/" component={Login} exact />
         <Route path="/gamelist" component={GameList} />
         <Route path="/playground" component={PlayGround} />
       </Switch>
-    </>
+    </BaseballProvider>
   );
 };
 
