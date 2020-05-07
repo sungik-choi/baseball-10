9f6aabfe73f4868aa46a25c03e9816485cb65f00
최장호 <hmchoi651@naver.com>
2020-05-07 14:48:55 +0900

    [#9] Docs : Mock data 형식 변경

--- FE/baseball/src/mock/baseballData.js
+++ FE/baseball/src/mock/baseballData.js
@@ -1,9 +1,25 @@
 const baseballData = {
-  gameList: [
-    { home: "Lions", away: "BEARS" },
-    { home: "Eagles", away: "Giants" },
-    { home: "Dinos", away: "Tigers" },
+  teamList: [
+    {
+      id: 1,
+      name: "LA다저스",
+      image: "",
+    },
+    {
+      id: 2,
+      name: "필라델피아",
+      image: "",
+    },
+    {
+      id: 3,
+      name: "LA에인절스",
+      image: "",
+    },
+    {
+      id: 4,
+      name: "뉴욕양키스",
+      image: "",
+    },
   ],
 };
-
 export default baseballData;
