0cf037b62e225284335f885df8930f2619e367cf
최장호 <hmchoi651@naver.com>
2020-05-07 16:38:10 +0900

    [#9] Feat : Dispatch로 초기값을 받아오는 Fetch Custom hook 생성

---
+++ FE/baseball/src/components/useFetch.jsx
@@ -0,0 +1,23 @@
+import { useEffect } from "react";
+
+// fetch와 reducer에 dispatch를 이용해서 init data를 받아오는 fetch custom hook
+// 인자로 url , actionType , dispatch 를 받아서 fetch 처리
+
+const useFetch = (url, actionType, dispatch) => {
+  console.log(`......... fetch loading`);
+  const initialFetch = async () => {
+    try {
+      const response = await fetch(url);
+      const initialData = await response.json();
+      dispatch({ type: actionType, data: initialData });
+    } catch (err) {
+      alert(`Fetch Error !!`);
+    }
+  };
+
+  useEffect(() => {
+    initialFetch();
+  }, []);
+};
+
+export default useFetch;
