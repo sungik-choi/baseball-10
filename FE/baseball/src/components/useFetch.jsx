48b3bcfc1988828c2f5dcd52fb92a312398ddd83
최장호 <hmchoi651@naver.com>
2020-05-07 20:05:35 +0900

    [#9] Docs : useFetch 설명 구문 추가

--- FE/baseball/src/components/useFetch.jsx
+++ FE/baseball/src/components/useFetch.jsx
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 
-// fetch와 reducer에 dispatch를 이용해서 init data를 받아오는 fetch custom hook
-// 인자로 url , actionType , dispatch 를 받아서 fetch 처리
+/* fetch와 reducer에 dispatch를 이용해서 init data를 받아오는 fetch custom hook
+ 인자로 url , actionType , dispatch 를 받아서 fetch 처리 */
 
 const useFetch = (url, actionType, dispatch) => {
   console.log(`......... fetch loading`);
