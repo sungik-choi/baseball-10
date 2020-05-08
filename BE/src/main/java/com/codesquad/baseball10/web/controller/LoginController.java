1fcfbbd35eaa4e33a3f943d3f370e6b775b5fa0b
hyunjun <guswns1659@gmail.com>
2020-05-08 16:08:57 +0900

    [#11] Feat : GitHub Oauth 테스트
    - LoginController 생성.
      - OauthCallback() 추가.

---
+++ BE/src/main/java/com/codesquad/baseball10/web/controller/LoginController.java
@@ -0,0 +1,28 @@
+package com.codesquad.baseball10.web.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+
+@RestController
+@RequestMapping("github")
+public class LoginController {
+    private final Logger logger = LoggerFactory.getLogger(LoginController.class);
+    private RestTemplate restTemplate;
+
+    @GetMapping("/oauth/callback")
+    public String OauthCallback(@RequestParam("code") String code,
+                                HttpServletResponse response) {
+        logger.info("code : {}", code);
+        Cookie cookie = new Cookie("code", code);
+        response.addCookie(cookie);
+        return code;
+    }
+}
