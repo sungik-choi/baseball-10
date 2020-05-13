6d1a6a7fb3928da57ee62bdf0aa2df7b260c7cdc
최장호 <hmchoi651@naver.com>
2020-05-13 18:50:05 +0900

    [#23] Docs : 필요없는 import문 삭제

--- FE/baseball/src/components/playground/playerList/PlayerList.jsx
+++ FE/baseball/src/components/playground/playerList/PlayerList.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import styled from "styled-components";
 import { useBaseballState, useBaseballDispatch } from "context/context";
 import useFetch from "components/useFetch";
