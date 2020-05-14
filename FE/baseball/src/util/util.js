d95fc7b525eb6e8c37be0051f4c28d9f71aa21fa
최장호 <hmchoi651@naver.com>
2020-05-14 16:06:42 +0900

    [#23] Feat : String으로 넘어온 boolean을 실제 boolean으로 변환시켜주는 로직 구현

--- FE/baseball/src/util/util.js
+++ FE/baseball/src/util/util.js
@@ -7,6 +7,9 @@ const _ = {
   deleteCookie: (key) => {
     document.cookie = key + "=; expires=Thu, 01 Jan 1999 00:00:10 GMT;";
   },
+  transformBooleanType: (target) => {
+    return Boolean(target === "true");
+  },
 };
 
 export default _;
