c6b2786b90ab905cbe296ff75a735dee45f5b483
Sungik Choi <tjddlr92@gmail.com>
2020-05-14 19:45:11 +0900

    [#14] Chore : 테스트를 위해 reducer, mockData 변경
    - 베이스 정보 변경

--- FE/baseball/src/store/baseballStore.js
+++ FE/baseball/src/store/baseballStore.js
@@ -254,9 +254,9 @@ const baseballData = {
       out: 2,
     },
     plates: {
-      baseFirst: "0",
-      baseSecond: "0",
-      baseThird: "0",
+      baseFirst: "1",
+      baseSecond: "1",
+      baseThird: "1",
     },
     display: [
       {
