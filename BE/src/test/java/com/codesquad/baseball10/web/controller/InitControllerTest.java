6fa8fb6dc273b2628371b3c9c45b5b87ad2471da
HYUNJUN <guswns1659@gmail.com>
2020-05-24 16:20:34 +0900

    [#25] Feat : test 주석 해제

--- BE/src/test/java/com/codesquad/baseball10/web/controller/InitControllerTest.java
+++ BE/src/test/java/com/codesquad/baseball10/web/controller/InitControllerTest.java
@@ -1,71 +1,71 @@
-//package com.codesquad.baseball10.web.controller;
-//
-//import com.codesquad.baseball10.domain.BasicTeam;
-//import com.codesquad.baseball10.domain.GameApplication;
-//import com.codesquad.baseball10.service.InitService;
-//import org.junit.Before;
-//import org.junit.Test;
-//import org.junit.runner.RunWith;
-//import org.slf4j.Logger;
-//import org.slf4j.LoggerFactory;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.boot.test.web.client.TestRestTemplate;
-//import org.springframework.boot.web.server.LocalServerPort;
-//import org.springframework.http.HttpStatus;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.test.context.junit4.SpringRunner;
-//
-//import static org.assertj.core.api.Assertions.assertThat;
-//
-//@RunWith(SpringRunner.class)
-//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-//public class InitControllerTest {
-//
-//    @LocalServerPort
-//    private int port;
-//
-//    private Logger logger = LoggerFactory.getLogger(InitControllerTest.class);
-//
-//    @Autowired
-//    private InitService initService;
-//
-//    @Autowired
-//    private TestRestTemplate restTemplate;
-//
-//    @Before
-//    public void setUp() {
-//        String url = "http://localhost:" + port + "/init";
-//        restTemplate.getForEntity(url, GameApplication.class);
-//    }
-//
-//    @Test
-//    public void initDataTest() {
-//        String url = "http://localhost:" + port + "/init";
-//
-//        ResponseEntity<GameApplication> responseEntity
-//                = restTemplate.getForEntity(url, GameApplication.class);
-//
-//        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
-//        assertThat(responseEntity.getBody().getName()).isEqualTo("baseball");
-//    }
-//
-//    @Test
-//    public void initPlayerDataTest() {
-//
-//        //given
-//        Long teamId = 2L;
-//        String teamName = "키움히어로즈";
-//        String playerName = "키르난데스";
-//        String teamLogoUrl = "https://i.ibb.co/4dyWvQq/kiwoom.png";
-//
-//        GameApplication gameApplication = initService.getGameApplication();
-//        BasicTeam saved = gameApplication.getBasicTeams().stream().filter(match -> match.getName().equals(teamName))
-//                .findFirst().orElseThrow(() -> new IllegalStateException("initPlayerDataTest : " + teamName + "팀은 없습니다."));
-//
-//        assertThat(saved.getId()).isEqualTo(teamId);
-//        assertThat(saved.getName()).isEqualTo(teamName);
-//        assertThat(saved.getLogoUrl()).isEqualTo(teamLogoUrl);
-//        assertThat(saved.getBasicPlayers().get(0).getName()).isEqualTo(playerName);
-//    }
-//}
+package com.codesquad.baseball10.web.controller;
+
+import com.codesquad.baseball10.domain.BasicTeam;
+import com.codesquad.baseball10.domain.GameApplication;
+import com.codesquad.baseball10.service.InitService;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class InitControllerTest {
+
+    @LocalServerPort
+    private int port;
+
+    private Logger logger = LoggerFactory.getLogger(InitControllerTest.class);
+
+    @Autowired
+    private InitService initService;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Before
+    public void setUp() {
+        String url = "http://localhost:" + port + "/init";
+        restTemplate.getForEntity(url, GameApplication.class);
+    }
+
+    @Test
+    public void initDataTest() {
+        String url = "http://localhost:" + port + "/init";
+
+        ResponseEntity<GameApplication> responseEntity
+                = restTemplate.getForEntity(url, GameApplication.class);
+
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(responseEntity.getBody().getName()).isEqualTo("baseball");
+    }
+
+    @Test
+    public void initPlayerDataTest() {
+
+        //given
+        Long teamId = 2L;
+        String teamName = "키움히어로즈";
+        String playerName = "키르난데스";
+        String teamLogoUrl = "https://i.ibb.co/4dyWvQq/kiwoom.png";
+
+        GameApplication gameApplication = initService.getGameApplication();
+        BasicTeam saved = gameApplication.getBasicTeams().stream().filter(match -> match.getName().equals(teamName))
+                .findFirst().orElseThrow(() -> new IllegalStateException("initPlayerDataTest : " + teamName + "팀은 없습니다."));
+
+        assertThat(saved.getId()).isEqualTo(teamId);
+        assertThat(saved.getName()).isEqualTo(teamName);
+        assertThat(saved.getLogoUrl()).isEqualTo(teamLogoUrl);
+        assertThat(saved.getBasicPlayers().get(0).getName()).isEqualTo(playerName);
+    }
+}
--- BE/src/test/java/com/codesquad/baseball10/web/controller/MockControllerTest.java
+++ BE/src/test/java/com/codesquad/baseball10/web/controller/MockControllerTest.java
@@ -1,84 +1,84 @@
-//package com.codesquad.baseball10.web.controller;
-//
-//import com.codesquad.baseball10.web.dto.responesDto.PlayersResponseDto;
-//import com.codesquad.baseball10.web.dto.responesDto.progress.ProgressResponseDto;
-//import com.codesquad.baseball10.web.dto.responesDto.TeamChoiceResponseDto;
-//import com.codesquad.baseball10.web.dto.responesDto.TeamsResponseDto;
-//import org.junit.Test;
-//import org.junit.runner.RunWith;
-//import org.slf4j.Logger;
-//import org.slf4j.LoggerFactory;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.boot.test.web.client.TestRestTemplate;
-//import org.springframework.boot.web.server.LocalServerPort;
-//import org.springframework.http.HttpStatus;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.test.context.junit4.SpringRunner;
-//
-//import static org.assertj.core.api.Assertions.assertThat;
-//
-//@RunWith(SpringRunner.class)
-//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-//public class MockControllerTest {
-//
-//    @LocalServerPort
-//    private int port;
-//
-//    @Autowired
-//    private TestRestTemplate restTemplate;
-//
-//    private Logger logger = LoggerFactory.getLogger(MockControllerTest.class);
-//
-//    @Test
-//    public void getTeamsTest() {
-//        String url = "http://15.164.34.6/api/mock/teams";
-//        ResponseEntity<TeamsResponseDto> responseEntity = restTemplate.getForEntity(url, TeamsResponseDto.class);
-//
-//        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
-//        assertThat(responseEntity.getBody().getStatus()).isEqualTo("200");
-//    }
-//
-//    @Test
-//    public void getPlayersTest() {
-//        String url = "http://15.164.34.6/api/mock/players";
-//        ResponseEntity<PlayersResponseDto> responseEntity = restTemplate.getForEntity(url, PlayersResponseDto.class);
-//
-//        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
-//        assertThat(responseEntity.getBody().getData().get(0).getTeamName()).isEqualTo("삼성");
-//    }
-//
-//    @Test
-//    public void teamChoiceTest() {
-//        String url = "http://15.164.34.6/api/mock/1";
-//
-//        ResponseEntity<TeamChoiceResponseDto> responseEntity
-//                = restTemplate.getForEntity(url, TeamChoiceResponseDto.class);
-//
-//        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
-//        assertThat(responseEntity.getBody().getTeam().getName()).isEqualTo("삼성");
-//    }
-//
-//    @Test
-//    public void getFirstProgressTest() {
-//        String url = "http://15.164.34.6/api/mock/first";
-//
-//        ResponseEntity<ProgressResponseDto> responseEntity
-//                = restTemplate.getForEntity(url, ProgressResponseDto.class);
-//
-//        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
-//        assertThat(responseEntity.getBody().getUserWhere()).isEqualTo("HOME");
-//    }
-//
-//    @Test
-//    public void getProgressTest() {
-//        String url = "http://15.164.34.6/api/mock/2/TOP";
-//
-//        ResponseEntity<ProgressResponseDto> responseEntity
-//                = restTemplate.getForEntity(url, ProgressResponseDto.class);
-//
-//        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
-//        assertThat(responseEntity.getBody().getMatchInfo().getCurrentInning()).isEqualTo("2");
-//        assertThat(responseEntity.getBody().getMatchInfo().getWhen()).isEqualTo("TOP");
-//    }
-//}
+package com.codesquad.baseball10.web.controller;
+
+import com.codesquad.baseball10.web.dto.responesDto.PlayersResponseDto;
+import com.codesquad.baseball10.web.dto.responesDto.progress.ProgressResponseDto;
+import com.codesquad.baseball10.web.dto.responesDto.TeamChoiceResponseDto;
+import com.codesquad.baseball10.web.dto.responesDto.TeamsResponseDto;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class MockControllerTest {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    private Logger logger = LoggerFactory.getLogger(MockControllerTest.class);
+
+    @Test
+    public void getTeamsTest() {
+        String url = "http://15.164.34.6/api/mock/teams";
+        ResponseEntity<TeamsResponseDto> responseEntity = restTemplate.getForEntity(url, TeamsResponseDto.class);
+
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(responseEntity.getBody().getStatus()).isEqualTo("200");
+    }
+
+    @Test
+    public void getPlayersTest() {
+        String url = "http://15.164.34.6/api/mock/players";
+        ResponseEntity<PlayersResponseDto> responseEntity = restTemplate.getForEntity(url, PlayersResponseDto.class);
+
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(responseEntity.getBody().getData().get(0).getTeamName()).isEqualTo("삼성");
+    }
+
+    @Test
+    public void teamChoiceTest() {
+        String url = "http://15.164.34.6/api/mock/1";
+
+        ResponseEntity<TeamChoiceResponseDto> responseEntity
+                = restTemplate.getForEntity(url, TeamChoiceResponseDto.class);
+
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(responseEntity.getBody().getTeam().getName()).isEqualTo("삼성");
+    }
+
+    @Test
+    public void getFirstProgressTest() {
+        String url = "http://15.164.34.6/api/mock/first";
+
+        ResponseEntity<ProgressResponseDto> responseEntity
+                = restTemplate.getForEntity(url, ProgressResponseDto.class);
+
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(responseEntity.getBody().getUserWhere()).isEqualTo("HOME");
+    }
+
+    @Test
+    public void getProgressTest() {
+        String url = "http://15.164.34.6/api/mock/2/TOP";
+
+        ResponseEntity<ProgressResponseDto> responseEntity
+                = restTemplate.getForEntity(url, ProgressResponseDto.class);
+
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(responseEntity.getBody().getMatchInfo().getCurrentInning()).isEqualTo("2");
+        assertThat(responseEntity.getBody().getMatchInfo().getWhen()).isEqualTo("TOP");
+    }
+}
